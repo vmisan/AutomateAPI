8f5252a710c2e1d62d0bcf40b498462f279561c1
viktar.misan <victar.misan@gmail.com>
2021-08-03 20:13:10 +0300

    final code for 10 task

--- ex10.py
+++ ex10.py
@@ -1,12 +1,10 @@
-charactersCount = 15
-phraseLength = len(input())
-print(phraseLength)
-print(type(phraseLength))
-if charactersCount > phraseLength:
-    assert charactersCount > phraseLength
-    print(f"Allowed characters count which is entered by user = {charactersCount} ")
-else:
-    print(f"User typed not allowed count if characters = {charactersCount}")
+class Test:
+    def test_check_number(self):
+        charactersCount = 15
+        phraseLength = len(input())
+        print(phraseLength)
+        print(type(phraseLength))
+        assert charactersCount > phraseLength, f"Allowed characters count which is entered by user = {charactersCount}"
 
 
 
