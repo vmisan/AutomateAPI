672b8b40411de229db3a5fe9a94617bc8e0048d5
viktar.misan <victar.misan@gmail.com>
2021-08-10 22:37:38 +0300

    added new assertions

--- lib/assertions.py
+++ lib/assertions.py
@@ -22,6 +22,25 @@ class Assertions:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
 
     @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}', but it exists"
+
+    @staticmethod
     def assert_code_status(response: Response, expected_status_code):
         assert response.status_code == expected_status_code, \
             f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
